b7b8218eebbbf6b90682023690d39e70807e2b2d
Phenca <soran81@free.fr>
2024-02-12 09:13:15 +0100

    CRUD

--- src/Controller/BirthdayController.php
+++ src/Controller/BirthdayController.php
@@ -2,19 +2,81 @@
 
 namespace App\Controller;
 
-use App\Form\BirthdayFormType;
+use App\Entity\Birthday;
+use App\Form\BirthdayType;
+use App\Repository\BirthdayRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
+#[Route('/birthday')]
 class BirthdayController extends AbstractController
 {
-    #[Route('/birthday', name: 'app_birthday')]
-    public function index(): Response
+    #[Route('/', name: 'app_birthday_index', methods: ['GET'])]
+    public function index(BirthdayRepository $birthdayRepository): Response
     {
-        $form = $this->createForm(BirthdayFormType::class);
         return $this->render('birthday/index.html.twig', [
-            'form' => $form->createView(),
+            'birthdays' => $birthdayRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_birthday_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $birthday = new Birthday();
+        $form = $this->createForm(BirthdayType::class, $birthday);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($birthday);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_birthday_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('birthday/new.html.twig', [
+            'birthday' => $birthday,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_birthday_show', methods: ['GET'])]
+    public function show(Birthday $birthday): Response
+    {
+        return $this->render('birthday/show.html.twig', [
+            'birthday' => $birthday,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_birthday_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Birthday $birthday, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(BirthdayType::class, $birthday);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_birthday_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('birthday/edit.html.twig', [
+            'birthday' => $birthday,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_birthday_delete', methods: ['POST'])]
+    public function delete(Request $request, Birthday $birthday, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$birthday->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($birthday);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_birthday_index', [], Response::HTTP_SEE_OTHER);
     }
 }
